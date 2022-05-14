41ab28946a8340f6e44cf5bc35ed515f11a9419d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-13 20:30:08 -0400

    Axios interceptor: add authorization header only if url is not login/register

--- src/api-config.ts
+++ src/api-config.ts
@@ -7,17 +7,20 @@ const apiConfig = axios.create({
 
 apiConfig.interceptors.request.use(
   (config) => {
-    if (config.headers) {
-      config.headers.Authorization = `Bearer ${
-        JSON.parse(localStorage.getItem("sms-cuba") ?? "{}").token
-      }`;
-    } else {
-      config.headers = {
-        Authorization: `Bearer ${
+    if (!config.url?.includes("login") && !config.url?.includes("register")) {
+      if (config.headers) {
+        config.headers.Authorization = `Bearer ${
           JSON.parse(localStorage.getItem("sms-cuba") ?? "{}").token
-        }`,
-      };
+        }`;
+      } else {
+        config.headers = {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("sms-cuba") ?? "{}").token
+          }`,
+        };
+      }
     }
+    return config;
   },
   (error) => Promise.reject(error)
 );
