4e621c61054d2b37f0bea66172c7825be210f3bc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-20 22:13:57 -0400

    Fixed the wrong display of navbar when header is collapsed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,67 +40,72 @@ function Header() {
     dispatch(changePage("LOGIN"));
   };
   return (
-    <Navbar light color="light" expand="lg">
-      <Container className="d-flex p-0">
-        <NavbarBrand>SMS Cuba Send</NavbarBrand>
-        <NavbarToggler onClick={toggleNav} className="ms-auto" />
-        <Collapse
-          navbar
-          isOpen={navExpanded}
-          className="ms-auto justify-content-end"
-        >
-          <Nav navbar tabs>
-            <NavItem>
-              <NavLink
-                active={currentPage === "CONTACTS"}
-                onClick={showContacts}
-                style={{ cursor: "pointer" }}
-              >
-                Contactos
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                active={currentPage === "SEND"}
-                onClick={showSend}
-                style={{ cursor: "pointer" }}
-              >
-                Enviar
-              </NavLink>
-            </NavItem>
-            {logged && (
+    <header
+      className="bg-light"
+      style={{ boxShadow: "rgb(0 0 0 / 15%) 0px 0.05rem 0.5rem 0.15px" }}
+    >
+      <Container>
+        <Navbar light color="light" expand="lg" container={false}>
+          <NavbarBrand>SMS Cuba Send</NavbarBrand>
+          <NavbarToggler onClick={toggleNav} className="ms-auto" />
+          <Collapse
+            navbar
+            isOpen={navExpanded}
+            className="ms-auto justify-content-end"
+          >
+            <Nav navbar tabs>
               <NavItem>
-                <NavLink onClick={closeSession} style={{ cursor: "pointer" }}>
-                  Salir
+                <NavLink
+                  active={currentPage === "CONTACTS"}
+                  onClick={showContacts}
+                  style={{ cursor: "pointer" }}
+                >
+                  Contactos
                 </NavLink>
               </NavItem>
-            )}
-            {!logged && (
-              <>
+              <NavItem>
+                <NavLink
+                  active={currentPage === "SEND"}
+                  onClick={showSend}
+                  style={{ cursor: "pointer" }}
+                >
+                  Enviar
+                </NavLink>
+              </NavItem>
+              {logged && (
                 <NavItem>
-                  <NavLink
-                    active={currentPage === "LOGIN"}
-                    onClick={showLogin}
-                    style={{ cursor: "pointer" }}
-                  >
-                    Entrar
+                  <NavLink onClick={closeSession} style={{ cursor: "pointer" }}>
+                    Salir
                   </NavLink>
                 </NavItem>
-                <NavItem>
-                  <NavLink
-                    active={currentPage === "REGISTER"}
-                    onClick={showRegister}
-                    style={{ cursor: "pointer" }}
-                  >
-                    Registro
-                  </NavLink>
-                </NavItem>
-              </>
-            )}
-          </Nav>
-        </Collapse>
+              )}
+              {!logged && (
+                <>
+                  <NavItem>
+                    <NavLink
+                      active={currentPage === "LOGIN"}
+                      onClick={showLogin}
+                      style={{ cursor: "pointer" }}
+                    >
+                      Entrar
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      active={currentPage === "REGISTER"}
+                      onClick={showRegister}
+                      style={{ cursor: "pointer" }}
+                    >
+                      Registro
+                    </NavLink>
+                  </NavItem>
+                </>
+              )}
+            </Nav>
+          </Collapse>
+        </Navbar>
       </Container>
-    </Navbar>
+    </header>
   );
 }
 
