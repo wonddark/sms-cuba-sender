25acea9a0d0601b926057ac53ecae654c90895db
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-26 12:47:02 -0400

    Disabled 'edit', 'delete' and 'send' buttons when not convenient

--- src/components/ContactActions.tsx
+++ src/components/ContactActions.tsx
@@ -1,12 +1,20 @@
 import { Button, Modal, ModalBody, ModalHeader } from "reactstrap";
 import ContactForm from "./ContactsForm";
 import React, { useState } from "react";
+import { useAppSelector } from "../hooks/store";
 
 function ContactActions() {
+  const { selectedContacts } = useAppSelector((state) => ({
+    selectedContacts: state.contacts.selected,
+  }));
   const [showContactForm, setShowContactForm] = useState(false);
   const toggleContactForm = () => {
     setShowContactForm(!showContactForm);
   };
+  const disableEdit =
+    selectedContacts.length === 0 || selectedContacts.length > 1;
+  const disableDelete = selectedContacts.length === 0;
+  const disableSend = selectedContacts.length === 0;
   return (
     <>
       <Modal isOpen={showContactForm} toggle={toggleContactForm} centered>
@@ -21,15 +29,15 @@ function ContactActions() {
         <i className="bi bi-plus-circle me-sm-2" />
         <span className="d-none d-sm-inline">Nuevo</span>
       </Button>
-      <Button color="light" size="small">
+      <Button color="light" size="small" disabled={disableEdit}>
         <i className="bi bi-pencil-square me-sm-2" />
         <span className="d-none d-sm-inline">Editar</span>
       </Button>
-      <Button color="light" size="small">
+      <Button color="light" size="small" disabled={disableDelete}>
         <i className="bi bi-trash3 mesm2" />
         <span className="d-none d-sm-inline">Eliminar</span>
       </Button>
-      <Button color="light" size="small">
+      <Button color="light" size="small" disabled={disableSend}>
         <i className="bi bi-send me-sm-2" />
         <span className="d-none d-sm-inline">Enviar mensaje</span>
       </Button>
