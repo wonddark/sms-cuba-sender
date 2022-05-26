7b9170a6eaa31f4a8eccbe8090b440830fc21905
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-26 17:32:49 -0400

    Enabled the store to save/load state to/from local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,28 @@ import contactsReducer from "./contactsSlice";
 import api from "./services/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
+export const STORAGE_KEY = "sms-cuba-sender";
+
+const saveState = (state: ReturnType<typeof store.getState>) => {
+  try {
+    const stateFilter = JSON.parse(JSON.stringify(state));
+    stateFilter.contacts.selected = [];
+    stateFilter.page = "HOME";
+    delete stateFilter.api;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(stateFilter));
+  } catch (e) {}
+};
+
+const persistedState = (() => {
+  try {
+    const rawState = localStorage.getItem(STORAGE_KEY);
+    if (rawState === null) return undefined;
+    return JSON.parse(rawState);
+  } catch (e) {
+    return undefined;
+  }
+})();
+
 const store = configureStore({
   reducer: {
     session: sessionReducer,
@@ -14,11 +36,13 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
+  preloadedState: persistedState,
 });
 
 setupListeners(store.dispatch);
-
-export default store;
+store.subscribe(() => saveState(store.getState()));
 
 export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
