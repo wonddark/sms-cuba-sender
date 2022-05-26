6ac0ef255c64e348521fd2a787b34b6fb73067da
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-26 17:26:07 -0400

    Nothing changed

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
